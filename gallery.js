bbc5eac69ce2250813969fe54c9a1c628b404eca
gking4 <gking4@mail.csuchico.edu>
2017-04-27 22:21:12 -0700

    Add files via upload
    Minor text changes.

--- gallery.js
+++ gallery.js
@@ -1,6 +1,14 @@
+var chico = ["Bidwell Park", "La Vista", "Campus", "Celestinos", "Konkow"];
 
+var current = (Math.floor(Math.random() * chico.length))
 
 function shuffleGallery()
 {
-
+  var rand = (Math.floor(Math.random() * chico.length));
+  while(current == rand)
+  {
+    rand = (Math.floor(Math.random() * chico.length));
+  }
+  current = rand;
+  console.log(chico[rand]);
 }
